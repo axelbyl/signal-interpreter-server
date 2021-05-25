e4e16f35d43e873ec6fa99ed41673ba6f8bc0c82
ABYLIN <axel.bylin@gmail.com>
2021-05-25 16:30:27 +0200

    First files to set structure of project

---
+++ src/routes.py
@@ -0,0 +1,17 @@
+# routes.py
+from flask import Flask, request
+
+signal_interpreter_app = Flask(__name__)
+
+
+@signal_interpreter_app.route("/", methods=["GET"])
+def hello():
+    return "Hello world!"
+
+
+@signal_interpreter_app.route("/", methods=["POST"])
+def mirror_data():
+    data = request.get_json()
+    return data
+
+
